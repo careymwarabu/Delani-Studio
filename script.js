93373aa9a0635f9988f848ffe29837bcca1b5021
careymwarabu <careyakmwarabu@gmail.com>
2021-03-02 01:17:09 +0300

    add script.js

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-window.addEventListener("DomContentLoded", (event) => {
-console.log("DOM fully loaded and parsed");
+$(document).ready(function() {
+    $(".designClick").click(function() {
+        $(".designVisible").toggle();
+        $(".designHidden").toggle();
+    })
+})
 
-let h1elements = document.querySelectorAll(h1);
-// h1elements[0].addEventListener("click",function () {
-//  alert("This is a heading tag");
-// }); 
-h1elements.forEach(function(h1element) {
-h1elements.addEventListener("click", function() {
-alert("This is a heading tag");
-});
-});
+$(document).ready(function() {
+    $(".developmentClick").click(function() {
+        $(".developmentVisible").toggle();
+        $(".developmentHidden").toggle();
+    })
+})
 
-// ${"h1"}.click (function () {
-//  alert("This is a heading tag.");
-// });
-
-// $(document).ready(function () {
-//   $("#color-picker").change(function () {
-
-});
-});
-
-// $("p").click (function () {
-//   alert("This is a paragraph.")
-});
-
-// $("img").click (function () {
-//   alert("This is an image")    
-});
-jQuery("img").click(function() {
-alert("This is an image");
-});
-});
-//$(document).ready(function() {
-$('#toggle-btn').on("click", function(e) {
-$('#delani').toggle('slow');
-});
-});
-}
+$(document).ready(function() {
+    $(".productManagementClick").click(function() {
+        $(".productManagementVisible").toggle();
+        $(".productManagementHidden").toggle();
+    })
+})
